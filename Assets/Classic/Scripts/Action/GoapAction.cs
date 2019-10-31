914834089fed7df8d96dea340c679b46d4b4cd3c
Zephyr Zhang <zephyr1125@gmail.com>
2019-10-31 17:34:59 +0800

    initial commit

---
+++ Assets/Classic/Scripts/Action/GoapAction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Classic.Action
+{
+    public abstract class GoapAction : MonoBehaviour
+    {
+        private HashSet<KeyValuePair<string, object>> _preconditions;
+        private HashSet<KeyValuePair<string, object>> effects;
+    }
+}
