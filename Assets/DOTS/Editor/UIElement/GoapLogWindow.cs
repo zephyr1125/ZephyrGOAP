4d3072ab3481d5e46198e07d34d07ef4c35f4637
Zephyr Zhang <zephyr1125@gmail.com>
2019-12-31 21:17:56 +0800

    绘制Nodes

--- Assets/DOTS/Editor/UIElement/GoapLogWindow.cs
+++ Assets/DOTS/Editor/UIElement/GoapLogWindow.cs
@@ -1,6 +1,8 @@
 using System.IO;
+using System.Reflection;
 using DOTS.Logger;
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -15,17 +17,42 @@ namespace DOTS.Editor.UIElement
         }
         
         private static GoapLog _log;
+        private int _currentResult;
+
+        private VisualTreeAsset _nodeVisualTree;
 
         private void OnEnable()
         {
-            var root = this.rootVisualElement;
-            var visualTree =
+            Init();
+        }
+
+        private void Init()
+        {
+            var windowVisualTree =
                 AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(
                     "Assets/DOTS/Editor/UIElement/UXML/window.uxml");
-            visualTree.CloneTree(root);
+            windowVisualTree.CloneTree(rootVisualElement);
             
-            root.Q<Button>("load-button").RegisterCallback<MouseUpEvent>(
-                evt => LoadLogFile());
+            _nodeVisualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(
+                "Assets/DOTS/Editor/UIElement/UXML/node.uxml");
+            
+            rootVisualElement.Q<Button>("load-button").RegisterCallback<MouseUpEvent>(
+                evt =>
+                {
+                    LoadLogFile();
+                    ConstructInfo();
+                    ConstructGraph();
+                });
+            rootVisualElement.Q<Button>("reset-button").RegisterCallback<MouseUpEvent>(
+                evt => Reset());
+        }
+        
+        private void Reset()
+        {
+            rootVisualElement.Clear();
+            _log = null;
+            
+            Init();
         }
 
         private void LoadLogFile()
@@ -37,6 +64,39 @@ namespace DOTS.Editor.UIElement
                 var textReader = new StreamReader(path);
                 var json = textReader.ReadToEnd();
                 _log = JsonUtility.FromJson<GoapLog>(json);
+            }
+        }
+
+        private void ConstructInfo()
+        {
+            if (_log == null) return;
+
+            rootVisualElement.Q<Label>("agent-name").text = _log.results[0].AgentName;
+        }
+
+        private void ConstructGraph()
+        {
+            if (_log == null) return;
+
+            var mainFrame = rootVisualElement.Q("main-frame");
+
+            ConstructNode(mainFrame, _log.results[_currentResult].GoalNodeView, 0);
+        }
+
+        private void ConstructNode(VisualElement frame, NodeView node, int id)
+        {
+            _nodeVisualTree.CloneTree(frame);
+            var nodeVE = frame.Q("frame");
+            nodeVE.style.left = 16 + node.Iteration * (160+16);
+            nodeVE.style.top = 16 + id * (80+16);
+            nodeVE.name = node.Name;
+            nodeVE.Q<Label>("name").text = node.Name;
+
+            if (node.Children == null) return;
+            for (var i = 0; i < node.Children.Count; i++)
+            {
+                var child = node.Children[i];
+                ConstructNode(frame, child, i);
             }
         }
     }
