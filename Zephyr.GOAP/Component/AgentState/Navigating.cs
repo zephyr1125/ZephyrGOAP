0533fb367cb84cd58c21e45cd9898a9ce211e353
Zephyr Zhang <zephyr1125@gmail.com>
2020-09-03 20:00:45 +0800

    各个order具体执行system支持order转阶段

--- Zephyr.GOAP/Component/AgentState/Navigating.cs
+++
@@ -1,9 +0,0 @@
-using Unity.Entities;
-
-namespace Zephyr.GOAP.Component.AgentState
-{
-    public struct Navigating : IComponentData, IAgentState
-    {
-        public Entity NodeEntity { get; set; }
-    }
-}
